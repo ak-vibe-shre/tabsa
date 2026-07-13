65653348af85ad28c77f690b2f7c58cfb46b5efc
akshay aradhya <akshay.aradhya.sp@gmail.com>
2026-07-14 02:19:10 +0530

    fix

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,6 @@ import { app } from './app.js';
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0",() => {
+   console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });
